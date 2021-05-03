62c217ab8f598ee409081b30356be9bb0ccac107
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-03 23:29:55 +0530

    Added: Employee Daily Wage is added

--- EmpWageComputationPart3.java
+++ EmpWageComputationPart3.java
@@ -2,16 +2,26 @@
 public class EmpWageComputationPart3 {
 	
 	private static final int IS_PRESENT = 1;
-	public static double empCheck = Math.floor(Math.random() * 10) % 2;
+	private int WAGE_PER_HOUR = 20;
+	private int FULL_DAY_HOUR = 8;
+	
+	public static double empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	//Constructor
+	public EmpWageComputationPart3(int WAGE_PER_HOUR, int FULL_DAY_HOUR) {
+		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
+		this.FULL_DAY_HOUR = FULL_DAY_HOUR;
+	}
 	
 	public static void main(String[] args) {
-			
-		if(empCheck == IS_PRESENT) {
-			System.out.println("Employee is present");
-		}
 		
-		else
+		if(empCheck == IS_PRESENT) {
+		EmpWageComputationPart3 empWage = new EmpWageComputationPart3(20, 8);
+		int dailyWages = (empWage.WAGE_PER_HOUR * empWage.FULL_DAY_HOUR);
+		System.out.println("Employee daily wages is: "+dailyWages);
+		}
+		else {
 			System.out.println("Employee is absent");
+		}
 	}
 
 }
